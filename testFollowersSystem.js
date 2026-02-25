53af5fe177d24bc2dc6b77be54acee6e9b3b8b71
Ameer Hamza <ameerhamza.codes@gmail.com>
2026-02-25 21:58:46 +0500

    fixes #123 - Add stub for followers system tests

---
+++ testFollowersSystem.js
@@ -0,0 +1,10 @@
+// Stub file for testFollowersSystem
+// These are development/debug utilities
+
+export const debugFollowersSystem = async (userId) => {
+  console.log('debugFollowersSystem called with:', userId);
+};
+
+export const testFirestoreConnection = async () => {
+  console.log('testFirestoreConnection called');
+};
